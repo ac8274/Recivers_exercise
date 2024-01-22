670712368e9cfc50aeb72dbe2a802d4254dd1af5
Magshimim <ac8274@bs.amalnet.k12.il>
2024-01-22 11:21:17 +0200

    created static reciver

---
+++ app/src/main/java/com/example/recivers_exercise/recivers_list/Device_turned_on_reciver.java
@@ -0,0 +1,15 @@
+package com.example.recivers_exercise.recivers_list;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class Device_turned_on_reciver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
